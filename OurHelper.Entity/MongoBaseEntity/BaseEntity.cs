c65fdac57a400d5ab95692da04251e736289f9d6
lorinzhang <lorinzhang@163.com>
2015-08-07 10:18:52 +0800

    add mogodb base entity

---
+++ OurHelper.Entity/MongoBaseEntity/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurHelper.Entity.MongoBaseEntity
+{
+    /// <summary>
+    /// 数据实体基类
+    /// </summary>
+    [Serializable]
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            this.ID = Guid.NewGuid();
+            this.CreateTime = DateTime.Now;
+            this.LastUpdateTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 数据库唯一ID
+        /// </summary> 
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// 当前记录的创建时间
+        /// </summary> 
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 当前记录最近一次更新时间
+        /// </summary> 
+        public DateTime LastUpdateTime { get; set; }
+    }
+}
