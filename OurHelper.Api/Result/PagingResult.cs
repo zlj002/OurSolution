e86d076634fc9c369a05764530237c5dbd1e743c
lorinzhang <lorinzhang@163.com>
2015-11-19 16:47:13 +0800

    page

---
+++ OurHelper.Api/Result/PagingResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurHelper.Api.Result
+{
+    [Serializable]
+    public class PagingResult<T>
+    {
+        /// <summary>
+        /// 数据库总记录数
+        /// </summary>
+        public long RecordCount { get; set; }
+        /// <summary>
+        /// 接口返回的 结果集
+        /// </summary>
+        public T[] ResultSet { get; set; }
+    }
+}
